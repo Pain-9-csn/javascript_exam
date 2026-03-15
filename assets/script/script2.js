d5d68f67148308d05a04bd4ad6e958f003c61c18
bousso7 <ndiayemamediarra3170@gmail.com>
2026-03-15 17:09:07 +0000

    Bousso (#4)
    * Merge pull request #1 from Pain-9-csn/Bousso (#1)
    
    * modifications
    
    * amelioration

---
+++ assets/script/script2.js
@@ -0,0 +1,17 @@
+// Bouton commencer maintenat et en savoir plus 
+document.querySelector(".btn1").addEventListener("click", () => {
+  window.location.href = "assets/templates/templates.html"; 
+ 
+});
+
+document.querySelector(".btn3").addEventListener("click", () => {
+  window.location.href = "assets/about.html"; 
+ 
+});
+
+// bouton voir tous les modèles
+document.querySelector(".btn-template").addEventListener("click", () => {
+  window.location.href = "assets/templates/templates.html"; 
+ 
+});
+
